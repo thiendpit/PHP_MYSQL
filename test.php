e0d98a0436a5c21b81a64ffe4fdfbadc086cb396
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-05-04 07:56:19 +0700

    test

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+//echo 'Thien kute!';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Tutorial</title>
+</head>
+<body>
+  <h1><?php echo 'Hello Thien!' ?></h1>
+</body>
+</html>
